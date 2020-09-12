cfad963408c5176305a47ee8c10a41238d2e465c
elfernandes <71161213+elfernandes@users.noreply.github.com>
2020-09-12 15:46:11 -0300

    Create login_token.py

---
+++ login_token.py
@@ -0,0 +1,6 @@
+#Input your spotify login
+#insire o seu login no spotify
+login = ""
+#para pegar o token utilizar o link: https://developer.spotify.com/console/post-playlists/
+#get the token using the link: https://developer.spotify.com/console/post-playlists/
+token = ""
